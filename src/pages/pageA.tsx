024a207697a708f782bbfce33b03e68a1bd3246a
Deepak Kumar <deekumar@microsoft.com>
2019-06-17 13:04:43 +0530

    Git ignore added

--- src/pages/pageA.tsx
+++ src/pages/pageA.tsx
@@ -86,6 +86,7 @@ export class PageA extends React.Component {
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
+              <br/><br/><br/><br/>
               <h2>Why do we use it?</h2>
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
@@ -97,7 +98,22 @@ export class PageA extends React.Component {
               uncover many web sites still in their infancy. Various versions
               have evolved over the years, sometimes by accident, sometimes on
               purpose (injected humour and the like).
+              <br/><br/><br/><br/>
               <h2>Where does it come from?</h2>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            <br/><br/><br/><br/>
+            <h2>Where does it come from?</h2>
               Contrary to popular belief, Lorem Ipsum is not simply random text.
               It has roots in a piece of classical Latin literature from 45 BC,
               making it over 2000 years old. Richard McClintock, a Latin
